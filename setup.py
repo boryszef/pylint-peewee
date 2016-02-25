20afae62a276217bf71740e547177928874237ca
Hayden Chudy <hayden.chudy@croscon.com>
2016-02-25 15:30:13 -0500

    Some classifiers for PyPI.

--- setup.py
+++ setup.py
@@ -3,12 +3,17 @@ from distutils.core import setup
 setup(
     name='pylint-peewee',
     py_modules=['pylint-peewee'],
-    version='0.1.3',
+    version='0.1.4',
     description='PyLint extensions for PeeWee',
     author='Hayden Chudy',
     author_email='hjc1710@gmail.com',
     url='https://github.com/hjc1710/pylint-peewee',
-    download_url='https://github.com/hjc1710/pylint-peewee/archive/v0.1.3.tar.gz',
+    download_url='https://github.com/hjc1710/pylint-peewee/archive/v0.1.4.tar.gz',
     keywords=['peewee', 'pylint', 'plugin',],
-    classifiers=[],
+    classifiers=[
+        "Environment :: Console",
+        "Programming Language :: Python :: 2.7",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Quality Assurance",
+    ],
 )
