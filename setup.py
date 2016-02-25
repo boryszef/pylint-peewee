6c66cfe0056fa31184ec81bcefdedc0d271c177d
Hayden Chudy <hayden.chudy@croscon.com>
2016-02-25 15:25:28 -0500

    Configure gitignore; version bump.

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='pylint-peewee',
     py_modules=['pylint-peewee'],
-    version='0.1.2',
+    version='0.1.3',
     description='PyLint extensions for PeeWee',
     author='Hayden Chudy',
     author_email='hjc1710@gmail.com',
     url='https://github.com/hjc1710/pylint-peewee',
-    download_url='https://github.com/hjc1710/pylint-peewee/archive/v0.1.2.tar.gz',
+    download_url='https://github.com/hjc1710/pylint-peewee/archive/v0.1.3.tar.gz',
     keywords=['peewee', 'pylint', 'plugin',],
     classifiers=[],
 )
