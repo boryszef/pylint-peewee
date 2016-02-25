5b78318fee4822e6b5d8804f4f5e7b47a86bea96
Hayden Chudy <hayden.chudy@croscon.com>
2016-02-25 15:17:26 -0500

    Properly reference the next coming tag.

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='pylint-peewee',
     packages=['pylint-peewee'],
-    version='0.1.0',
+    version='0.1.2',
     description='PyLint extensions for PeeWee',
     author='Hayden Chudy',
     author_email='hjc1710@gmail.com',
     url='https://github.com/hjc1710/pylint-peewee',
-    download_url='https://github.com/hjc1710/pylint-peewee/archive/v0.1.0.tar.gz',
+    download_url='https://github.com/hjc1710/pylint-peewee/archive/v0.1.2.tar.gz',
     keywords=['peewee', 'pylint', 'plugin',],
     classifiers=[],
 )
