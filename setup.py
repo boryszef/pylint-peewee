40a6695cb954c3e545cdd0a9a16306919fff2f11
Hayden Chudy <hayden.chudy@croscon.com>
2016-02-25 15:13:50 -0500

    A nice little setup.py for you.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='pylint-peewee',
+    packages=['pylint-peewee'],
+    version='0.1.0',
+    description='PyLint extensions for PeeWee',
+    author='Hayden Chudy',
+    author_email='hjc1710@gmail.com',
+    url='https://github.com/hjc1710/pylint-peewee',
+    download_url='',
+    keywords=['peewee', 'pylint', 'plugin',],
+    classifiers=[],
+)
