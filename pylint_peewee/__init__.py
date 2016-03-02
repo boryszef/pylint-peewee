9fac449a4492f90d73dd8412712ad14a8f0600aa
Hayden Chudy <hayden.chudy@croscon.com>
2016-03-02 16:41:11 -0500

    Support missing `id` field and `create` method.
    Better stubbing of all methods.
    
    We now properly stub out the `id` field (which automatically returns a mocked
      PrimaryKeyField) and we stub out the `creates` method, which returns an
      instance of the current class.
    
    All mocked objects, now return their mocked query. Previously they were merely
      MOCKED to their query and did not return it. This made the linter work, but
      could likely lead to edge case errors (e.g., these queries aren't callable,
      but the methods they mock are and a complex call chain may not be properly
      mocked). I'm just theorizing here, but it seemed like the better thing to do.
    
    TONS more mocks in mocks.py.

--- pylint_peewee/__init__.py
+++ pylint_peewee/__init__.py
@@ -1,24 +1,33 @@
 from astroid import MANAGER
 from astroid import scoped_nodes
 
-from .mocks import DummySelect, DummyInsert, DummyDelete, DummyUpdate
+from .mocks import (DummySelect, DummyInsert, DummyDelete, DummyUpdate,
+                    DummyPrimaryKeyField)
 
 
 def register(linter):
     pass
 
 
+def _returns(ret_val):
+    return lambda: ret_val
+
+
 def transform(cls):
     if 'db.Model' in cls.basenames:
         overrides = {
-            'select': DummySelect,
-            'get': DummySelect,
-            'insert': DummyInsert,
-            'update': DummyUpdate,
-            'delete': DummyDelete,
+            'select': _returns(DummySelect()),
+            'get': _returns(DummySelect()),
+            'insert': _returns(DummyInsert()),
+            'update': _returns(DummyUpdate()),
+            'delete': _returns(DummyDelete()),
+            # @TODO: There has to be a better way to see if the id field is
+            # missing or just not needed
+            'id': DummyPrimaryKeyField(),
+            'create': _returns(cls.instantiate_class()),
         }
         for key in overrides:
-            cls.locals[key] = [overrides[key]()]
+            cls.locals[key] = [overrides[key]]
 
 
 MANAGER.register_transform(scoped_nodes.Class, transform)
--- pylint_peewee/mocks.py
+++ pylint_peewee/mocks.py
@@ -1,6 +1,144 @@
+_sentinel = object()
 
 
-class DummyQuery(object):
+class BaseMock(object):
+    methods = ()
+
+    def passer(self, *args, **kwargs):
+        pass
+
+    def __init__(self):
+        self.register(self.methods)
+
+    def register(self, methods, stub=_sentinel):
+        if stub is _sentinel:
+            stub = self.passer
+
+        for m in methods:
+            setattr(self, m, stub)
+
+
+class DummyNode(BaseMock):
+    _node_type = 'node'
+
+    methods = (
+        'c',
+        'extend',
+        'clone_base',
+        'clone',
+        '__invert__',
+        'alias',
+        'bind_to',
+        'asc',
+        'desc',
+        '__pos__',
+        '__neg__',
+        '_e',
+        '__and__',
+        '__or__',
+        '__add__',
+        '__sub__',
+        '__mul__',
+        '__div__',
+        '__xor__',
+        '__radd__',
+        '__rsub__',
+        '__rmul__',
+        '__rdiv__',
+        '__rand__',
+        '__ror__',
+        '__rxor__',
+        '__eq__',
+        '__ne__',
+        '__lt__',
+        '__le__',
+        '__gt__',
+        '__ge__',
+        '__lshift__',
+        '__rshift__',
+        '__mod__',
+        '__pow__',
+        'bin_and',
+        'bin_or',
+        'in_',
+        'not_in',
+        'is_null',
+        'contains',
+        'startswith',
+        'endswith',
+        'between',
+        'regexp',
+        'concat',
+    )
+
+
+DummySQL = DummyNode
+
+
+class DummyEntity(DummyNode):
+    def __init__(self):
+        super(DummyEntity, self).__init__()
+
+        new_methods = ('__getattr__',)
+
+        self.register(new_methods)
+
+
+class DummyField(DummyNode):
+    _field_counter = 0
+    _order = 0
+    _node_type = 'field'
+    db_field = 'unknown'
+
+    def __init__(self):
+        super(DummyField, self).__init__()
+
+        fields = (
+            'null',
+            'index',
+            'unique',
+            'verbose_name',
+            'help_text',
+            'db_column',
+            'default',
+            'choices',
+            'primary_key',
+            'squencee,'
+            'constraints',
+            'schema',
+            '_is_bound'
+        )
+
+        self.register(fields, None)
+
+        new_methods = (
+            'add_to_class',
+            'get_database',
+            'get_column_type',
+            'get_db_field',
+            'get_modifiers',
+            'coerce',
+            'db_value',
+            'python_value',
+            'as_entity',
+            '__ddl_column__',
+            '__ddl__',
+            '__hash__',
+        )
+
+        self.register(new_methods)
+
+
+class DummyIntegerField(DummyField):
+    db_field = 'int'
+    coerce = int
+
+
+class DummyPrimaryKeyField(DummyIntegerField):
+    db_field = 'primary_key'
+
+
+class DummyQuery(BaseMock):
     """Mock for pylint"""
     require_commit = None
 
@@ -24,16 +162,6 @@ class DummyQuery(object):
         'where',
         '_model_shorthand'
     )
-
-    def passer(self, *args, **kwargs):
-        pass
-
-    def __init__(self):
-        self.register(self.methods)
-
-    def register(self, methods):
-        for m in methods:
-            setattr(self, m, self.passer)
 
 
 class DummySelect(DummyQuery):
